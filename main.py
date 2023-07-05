fa5e9e78b0cc41007db3d89715949b93ef366c77
KgotsoPhela <90217020+KgotsoPhela@users.noreply.github.com>
2023-07-05 13:16:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ with st.expander('Analyze CSV'):
         df['analysis'] = df['score'].apply(analyze)
         st.write(df.head(10))
 
-        #@st.cache
-        @ st.cache_data
+        @st.cache
+        #@ st.cache_data
         def convert_df(df):
             # IMPORTANT: Cache the conversion to prevent computation on every rerun
             return df.to_csv(index=False).encode('utf-8')
