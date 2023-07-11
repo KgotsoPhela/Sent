2977f57362b16508db2a0a5497ae79b06c5a381d
KgotsoPhela <90217020+KgotsoPhela@users.noreply.github.com>
2023-07-11 13:50:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,45 @@ import pandas as pd
 import streamlit as st
 import cleantext
 from io import StringIO
-import pdfkit
-import os
-import tempfile
-import plotly.graph_objects as go
-#from streamlit_option_menu import option_menu
 
-# Set up the sidebar menu
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Main Menu",
-        options=["Home", "Analysis", "About"],
-        icons=["house", "bar-chart", "book"],
-        menu_icon="list"
-    )
+# FIRST EXPANDER
+st.header('Lubanzi Sentiment Analysis')
+with st.expander('Analyze Text'):
+    text = st.text_input('Text here: ')
+    if text:
+        blob = TextBlob(text)
+        st.write('Polarity: ', round(blob.sentiment.polarity,2))
+        st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
 
-# Main app content
-if selected == "Analysis":
-    st.header('Lubanzi Sentiment Analysis')
-    selected = option_menu(
-        menu_title=None,
-        options=["Analyze Text", "Analyze CSV"],
-        icons=["activity", "bar-chart"],
-        orientation="horizontal"
-    )
+
+  #  pre = st.text_input('Clean Text: ')
+  #  if pre:
+  #      st.write(cleantext.clean(pre, clean_all=False, extra_spaces=True,
+  #                               stopwords=True, lowercase=True, numbers=True, punct=True))
+
+
+                                             # SECOND EXPANDER
+with st.expander('Analyze CSV'):
+
+    st.write("The uploaded CSV file should be in the following format...")
+    data = {
+    'ID': range(1, 3),
+    'tweets':[
+        "Message or customer feedback number 1",
+        "Message or customer feedback number 2",    ]
+    }
+    df = pd.DataFrame(data)
+    st.dataframe(df)
+    upl = st.file_uploader('Upload file')
+
+    def score(x):
+        blob1 = TextBlob(x)
+        return blob1.sentiment.polarity
+
+    def analyze(x):
+        if x >= 0.5:
+            return 'Positive'
+        elif x <= -0.2:
+            return 'Negative'
+        else:
+            return 'Neutral'
