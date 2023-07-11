6199852e205cdce38e46dd857d4d8595f96fd8e8
KgotsoPhela <90217020+KgotsoPhela@users.noreply.github.com>
2023-07-11 14:50:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ if selected == "Analysis":
         #            punct=True,
         #        )
         #    )
-    elif selecte == "Quick guide":
+    elif selected == "Quick guide":
         st.write("**Polarity:** Positive values indicates positive sentiment, negative values for negative sentiment and 0 for Neutral.")
         st.write("**Subjectivity:** Measures how subjective or objective the text is. High subjectivity score indicates that the text is more opinionated or subjective, low score suggests a more objective or factual nature of the text.")
 
